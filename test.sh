3e2b5c36ce10004c3a79b2781565a817a74f5a40
Choi.junwhan <paul911014@naver.com>
2020-07-15 16:42:07 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-mkdir test
+for i in "$@"
+do
+case $i in
+        --instance-name=*)
+        INSTANCENAME="${i#*=}"
+        shift
+        ;;
+        --api-token=*)
+        APITOKEN="${i#*=}"
+        shift
+        ;;
+esac
+done
+
+echo "INSTANCENAME      = ${INSTANCENAME}"
+echo "APITOKEN          = ${APITOKEN}"
+
+kubectl create ns nexclipper
+
