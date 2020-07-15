5cf7cd348753e7d16cd3d197fdefa5daddbad529
Choi.junwhan <paul911014@naver.com>
2020-07-15 16:57:17 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,11 +11,23 @@ case $i in
         APITOKEN="${i#*=}"
         shift
         ;;
+        --user-name=*)
+        USERNAME="${i#*=}"
+        shift
+        ;;
 esac
 done
 
 echo "INSTANCENAME      = ${INSTANCENAME}"
 echo "APITOKEN          = ${APITOKEN}"
+echo "USERNAME          = ${USERNAME}"
 
 kubectl create ns nexclipper
 
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: ${USERNAME}
+  namespace: nexclipper
+EOF
