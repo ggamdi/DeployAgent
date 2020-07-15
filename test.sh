d876b3c8a17626da7c93918cf7d4cf7475ccdc3a
Choi.junwhan <paul911014@naver.com>
2020-07-15 17:13:23 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -30,4 +30,49 @@ kind: ServiceAccount
 metadata:
   name: ${USERNAME}
   namespace: nexclipper
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  namespace: nexclipper
+  name: nex-secrets
+  labels:
+    app.kubernetes.io/name: nexclipper-kubernetes-agent
+stringData:
+  nexclipper-api-tokey: ${APITOKEN}
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: nexclipper
+  name: nexclipper-agent-config
+    labels:
+        app.kubernetes.io/name: nexclipper-kubernetes-agent
+data:
+    instance-name: ${INSTANCENAME}
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  namespace: nexclipper
+  name: nexclipper-role
+rules:
+- apiGroups: [""]
+  resources: ["pods"] # Object 지정
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"] # Action 제어 
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  namespace: nexclipper
+  name: nexclipper-rb
+subjects:
+- kind: ServiceAccount
+  name: ${USERNAME}
+  namespace: nexclipper
+roleRef:
+  kind: Role 
+  name: nexclipper-role
+  apiGroup: rbac.authorization.k8s.io
+---
 EOF
